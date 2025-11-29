971da4562c3ca80f6f07c06969b66fb728e95154
Pravendra jain <pravendrajain421@gmail.com>
2025-11-29 17:35:28 +0530

    make the connection in the redis

--- socket/SocketHandler.js
+++ socket/SocketHandler.js
@@ -1,21 +1,55 @@
 import { Server } from "socket.io";
-export const initializeSocketIo =(httpServer)=>{
-    const io = new Server(httpServer,{
-        cors:{
-            origin:"http://localhost:5173",
-            methods:["GET","POST","PUT","DELETE"],
-            credentials:true
+import Redis from "ioredis";
+
+export const initializeSocketIo = (httpServer) => {
+    const io = new Server(httpServer, {
+        cors: {
+            origin: "http://localhost:5173",
+            methods: ["GET", "POST", "PUT", "DELETE"],
+            credentials: true
         }
     });
-    io.on("connection",(socket)=>{
-        console.log(`Socket Connected Successfully. ID:${socket.id}`);
-        socket.on("send message",(data)=>{
-            console.log(`message recieved from ${socket.id}:${data.text}`);
-            socket.broadcast.emit("recieve message",data)
+
+    const redis = new Redis({
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT,
+        password: process.env.REDIS_PASSWORD,
+        username: "default"
+    });
+
+    redis.on("connect", () => console.log("✅ Connected to Redis Phonebook"));
+    redis.on("error", (error) => console.log("❌ Redis Error", error));
+
+    io.on("connection", (socket) => {
+        
+        // 1. ADD USER TO REDIS
+        socket.on("add-user", async (userId) => {
+            if(userId){
+                await redis.set(`user:${userId}`, socket.id);
+                socket.userId = userId; // Save to socket object for disconnect
+                // console.log(`User ${userId} mapped to ${socket.id}`);
+            }
+        });
+
+        // 2. SEND MESSAGE
+        socket.on("send-msg", async (data) => {
+            // data = { to, from, msg }
+            const sendUserSocket = await redis.get(`user:${data.to}`);
+
+            if (sendUserSocket) {
+                socket.to(sendUserSocket).emit("msg-recieve",{
+                    from:data.from,
+                    message:data.msg
+                });
+            }
         });
         
-        socket.on("disconnect",()=>{
-            console.log(`user Disconnected Id :${socket.id}`);
+        // 3. CLEANUP ON DISCONNECT
+        socket.on("disconnect", async () => {
+            if(socket.userId){
+                await redis.del(`user:${socket.userId}`);
+                console.log(`User ${socket.userId} disconnected`);
+            }
         });
     });
     return io;
